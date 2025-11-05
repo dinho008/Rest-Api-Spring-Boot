28ab3ccd1c520ce935336917dbe99123e52394c6
dinho08 <cardoso.gui.2015@gmail.com>
2025-11-05 19:07:07 -0300

    greeting Controller

---
+++ api-example/src/main/java/br/edu/atitus/api_example/entities/UserEntity.java
@@ -0,0 +1,58 @@
+package br.edu.atitus.api_example.entities;
+
+import java.util.UUID;
+
+public class UserEntity {
+
+	private UUID id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String passwaord;
+	
+	private TypeUser type;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswaord() {
+		return passwaord;
+	}
+
+	public void setPasswaord(String passwaord) {
+		this.passwaord = passwaord;
+	}
+
+	public TypeUser getType() {
+		return type;
+	}
+
+	public void setType(TypeUser type) {
+		this.type = type;
+	}
+	
+	
+}
